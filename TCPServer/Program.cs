4f44acb3d48e32877ee6b04c0cbad2cf45dc8b04
GaoHeng <425168464@qq.com>
2017-07-12 13:30:51 +0800

    test socket tcp

---
+++ TCPServer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPServer
+{
+    class Program
+    {
+        public static int maxClient = 0;
+        static void Main(string[] args)
+        {
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Socket clientSocket = null;
+            IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 88);
+            serverSocket.Bind(ipEndPoint);
+            serverSocket.Listen(maxClient);
+            clientSocket = serverSocket.Accept();
+
+            string msg = "hello client ! 你好......";
+            byte[] data = Encoding.UTF8.GetBytes(msg);
+            clientSocket.Send(data);
+
+            byte[] receiveBuffer = new byte[1024];
+            int count = clientSocket.Receive(receiveBuffer);
+            string receiveMsg = Encoding.UTF8.GetString(receiveBuffer, 0, count);
+            Console.WriteLine(receiveMsg);
+
+            clientSocket.Close();
+            serverSocket.Close();
+            Console.ReadKey();
+        }
+    }
+}
