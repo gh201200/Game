b4e73b61d81d209f06e3d7ef5d048a8d816af13f
GaoHeng <425168464@qq.com>
2017-07-13 12:14:09 +0800

    add TEST

---
+++ TEST/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] data = new byte[1024];
+            byte[] buff1 = Encoding.UTF8.GetBytes("中文English123 ");
+            byte[] buff2 = BitConverter.GetBytes(0);
+            data = buff1.Concat(buff2).ToArray();
+            foreach (byte b in data)
+            {
+                Console.Write(b + ":");
+            }
+            Console.ReadKey();
+        }
+    }
+}
