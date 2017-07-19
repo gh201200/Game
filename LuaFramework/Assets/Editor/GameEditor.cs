c659006dc325e9dfcb5201b408b043b9e6951433
GaoHeng <425168464@qq.com>
2017-07-19 21:06:15 +0800

    update

--- LuaFramework/Assets/Editor/GameEditor.cs
+++ LuaFramework/Assets/Editor/GameEditor.cs
@@ -8,10 +8,6 @@ public class GameEditor
     [MenuItem("Tool/Test", false, 0)]
     static void Test()
     {
-        string url = @"http://localhost/ShareFolder/test.sql";
-        using (WWW www = new WWW(url))
-        {
-
-        }
+        Debug.Log(Application.dataPath + "/..");
     }
 }
