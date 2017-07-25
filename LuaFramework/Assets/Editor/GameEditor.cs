450560a6194c5f024734c3aa493236c2a9bb278c
GaoHeng <425168464@qq.com>
2017-07-25 20:09:36 +0800

    update

--- LuaFramework/Assets/Editor/GameEditor.cs
+++ LuaFramework/Assets/Editor/GameEditor.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using SLua;
 using UnityEditor;
 using Object = UnityEngine.Object;
+using Debug = UnityEngine.Debug;
+using ICSharpCode.SharpZipLib.Zip;
+using System.Diagnostics;
+using System.Timers;
 
 public class GameEditor
 {
@@ -15,10 +19,67 @@ public class GameEditor
     [MenuItem("Tool/Test", false, 1000)]
     static void Test()
     {
-        GetAllAssets();
+        Stopwatch sp = new Stopwatch();
+        sp.Start();
+        //Debug.Log("begin compress file");
+        //string path = @"C:\Users\Administrator\Desktop\PDF\Android开发入门教程.pdf";
+        //ZipUtil.PackFile(path, @"C:\Users\Administrator\Desktop\test.zip", (cur, total) =>
+        //{
+        //    string content = (((double)cur / total) * 100).ToString("0.0") + "% " + (cur / 1024d / 1024d).ToString("0.0") + "M/" + (total / 1024d / 1024d).ToString("0.0") + "M";
+        //    Debug.Log(content);
+        //}, () =>
+        //{
+        //    Debug.Log("compress finish! 耗时:" + sp.Elapsed.TotalSeconds + "s");
+        //    sp.Stop();
+        //}, 9, "123456");
+
+        //Debug.Log("begin compress file");
+        //string path = @"D:\SocketPrograming\SocketPrograming\ScrollView";
+        //ZipUtil.PackDirectory(path, @"C:\Users\Administrator\Desktop\test.zip", (cur, total) =>
+        //{
+        //    string content = (((double)cur / total) * 100).ToString("0.0") + "%    " + (cur / 1024d / 1024d).ToString("0.0") + "M/" + (total / 1024d / 1024d).ToString("0.0") + "M";
+        //    Debug.Log(content);
+        //}, () =>
+        //{
+        //    Debug.Log("compress finish! 耗时:" + sp.Elapsed.TotalSeconds + "s");
+        //});
+
+        //string pach = @"E:\美术策划资源\美术资源.zip";
+        //ZipUtil.UnpackFileAsync(@"C:\Users\Administrator\Desktop\test", pach, (cur, total) =>
+        //{
+        //    string content = (((double)cur / total) * 100).ToString("0.0") + "%    " + (cur / 1024d / 1024d).ToString("0.0") + "M/" + (total / 1024d / 1024d).ToString("0.0") + "M";
+        //    Debug.Log(content);
+        //}, () => Debug.Log("unpack finish! 耗时:" + sp.Elapsed.TotalSeconds + "s"));
+
+        string content = "";
+        float progress = 0f;
+        try
+        {
+            ZipUtil.UnpackFile(@"C:\Users\Administrator\Desktop\test", Config.StreamingAssetsPath + "/data.zip",
+                (cur, total) =>
+                {
+                    progress = (float)cur / total;
+                    content = (progress * 100).ToString("0.0") + "% " + (cur / 1024d / 1024d).ToString("0.0") + "M/" +
+                              (total / 1024d / 1024d).ToString("0.0") + "M";
+                    EditorUtility.DisplayProgressBar("progress", content, progress);
+                },
+                () =>
+                {
+                    Debug.Log("unpack finish! 耗时:" + sp.Elapsed.TotalSeconds + "s");
+                });
+        }
+        catch (Exception e)
+        {
+            throw new Exception(e.Message);
+        }
+        finally
+        {
+            EditorUtility.ClearProgressBar();
+            AssetDatabase.Refresh();
+        }
     }
 
-    [MenuItem("Tool/BuildSetting", false, 0)]
+    [MenuItem("Tool/BuildSetting", false, 1)]
     static void BuildSetting()
     {
         Object obj = GetBuildSettingAsset();
@@ -26,7 +87,7 @@ public class GameEditor
         Selection.activeObject = obj;
     }
 
-    [MenuItem("Tool/AssetBundle/Build Single", false, 100)]
+    [MenuItem("Tool/AssetBundle/Build Single", false, 10)]
     static void BuildSingle()
     {
         AddResVersion();
@@ -37,52 +98,50 @@ public class GameEditor
 #endif
     }
 
-    [MenuItem("Tool/AssetBundle/Build All", false, 0)]
+    [MenuItem("Tool/AssetBundle/Build All", false, 20)]
     static void BuildAll()
     {
         BuildSetting bs = GetBuildSettingAsset();
         HashSet<string> files = GetAllAssets();
-        string subFolder = "";
         AddResVersion();
+
 #if PerFile
         //每个文件是一个AssetBundle
         foreach (string str in files)
         {
             string fileName = Path.GetFileName(str);
-            string bundleTag = str.Replace(GetBuildSettingAsset().resPath, "").Replace(fileName, "") + Path.GetFileNameWithoutExtension(str) + ".bytes";
+            string bundleTag = str.Replace("Assets/", "").Replace(fileName, "") + Path.GetFileNameWithoutExtension(str) + ".bytes";
             AssetImporter ai = AssetImporter.GetAtPath(str);
             ai.assetBundleName = bundleTag;
             ai.assetBundleVariant = null;
         }
-        subFolder = "PerFile/";
 #else
         //每个文件夹下的文件是一个AssetBundle
         foreach (string str in files)
         {
             string fileName = Path.GetFileName(str);
-            string bundleTag = str.Replace(GetBuildSettingAsset().resPath, "").Replace("/" + fileName, "") + ".bytes";
+            string bundleTag = str.Replace("Assets/", "").Replace("/" + fileName, "") + ".bytes";
             AssetImporter ai = AssetImporter.GetAtPath(str);
             ai.assetBundleName = bundleTag;
             ai.assetBundleVariant = null;
         }
-        subFolder = "PerFolder/";
 #endif
-        string buildPath = Application.dataPath.Replace("/Assets", "/") + bs.buildPath + subFolder;
+        string buildPath = Application.dataPath.Replace("/Assets", "/") + bs.buildPath;
         DirectoryInfo di = new DirectoryInfo(buildPath);
         if (!di.Exists) di.Create();
-        BuildPipeline.BuildAssetBundles(bs.buildPath + subFolder, BuildAssetBundleOptions.ChunkBasedCompression | BuildAssetBundleOptions.DeterministicAssetBundle, bs.buildTarget);
+        BuildPipeline.BuildAssetBundles(bs.buildPath, BuildAssetBundleOptions.ChunkBasedCompression | BuildAssetBundleOptions.DeterministicAssetBundle, bs.buildTarget);
         AssetDatabase.RemoveUnusedAssetBundleNames();
         AssetDatabase.Refresh();
     }
 
-    [MenuItem("Tool/AssetBundle/Remove Unused AssetBundle Name", false, 0)]
+    [MenuItem("Tool/AssetBundle/Remove Unused AssetBundle Name", false, 30)]
     static void RemoveUnusedAssetBundleName()
     {
         AssetDatabase.RemoveUnusedAssetBundleNames();
         Debug.Log("clear success!");
     }
 
-    [MenuItem("Tool/AssetBundle/Remove All AssetBundle Name", false, 0)]
+    [MenuItem("Tool/AssetBundle/Remove All AssetBundle Name", false, 40)]
     static void RemoveAllAssetBundleName()
     {
         foreach (string file in GetAllAssets())
@@ -94,7 +153,7 @@ public class GameEditor
         RemoveUnusedAssetBundleName();
     }
 
-    [MenuItem("Tool/AssetBundle/Clear ResVersion Record", false, 0)]
+    [MenuItem("Tool/AssetBundle/Clear ResVersion Record", false, 50)]
     static void ClearResVersionRecord()
     {
         EditorPrefs.DeleteKey("BuildVersion");
@@ -102,10 +161,72 @@ public class GameEditor
         Debug.Log("clear success!");
     }
 
+    [MenuItem("Tool/AssetBundle/Clear AssetBundles", false, 60)]
+    static void ClearAssetBundles()
+    {
+        BuildSetting bs = GetBuildSettingAsset();
+        string path = Application.dataPath.Replace("Assets", "") + bs.buildPath;
+        path = path.Replace('/', '\\');
+        foreach (
+            var file in
+                Directory.GetDirectories(path, "*",
+                    SearchOption.TopDirectoryOnly))
+        {
+            if (file.Contains("LuaScripts")) continue;
+            Directory.Delete(file, true);
+        }
+        path = Path.GetDirectoryName(path);
+        int index = path.LastIndexOf('\\');
+        string name = path.Substring(index + 1);
+        string f1 = path + "\\" + name;
+        string f2 = path + "\\" + name + ".manifest";
+        if (File.Exists(f1)) File.Delete(f1);
+        if (File.Exists(f2)) File.Delete(f2);
+        AssetDatabase.Refresh();
+
+    }
+
     [MenuItem("Tool/Compile LuaScripts", false, 100)]
     static void CompileLuaScripts()
     {
         LuajitGen.exportLuajit(Config.LuaPath.Replace(Application.dataPath.Replace("Assets", ""), ""), "*.lua", GetBuildSettingAsset().buildPath + "LuaScripts/", JITBUILDTYPE.X86);
+    }
+
+    [MenuItem("Tool/Pack Assets", false, 200)]
+    static void PackAssets()
+    {
+        Debug.Log("begin pack");
+        Stopwatch sp = new Stopwatch();
+        sp.Start();
+        BuildSetting bs = GetBuildSettingAsset();
+        string path = Application.dataPath.Replace("Assets", "") + bs.buildPath;
+        path = path.Replace('/', '\\');
+
+        string content = "";
+        float progress = 0f;
+        try
+        {
+            ZipUtil.PackDirectory(path, Config.StreamingAssetsPath + "/data.zip", (cur, total) =>
+            {
+                progress = (float)cur / total;
+                content = (progress * 100).ToString("0.0") + "% " + (cur / 1024d / 1024d).ToString("0.0") + "M/" +
+                          (total / 1024d / 1024d).ToString("0.0") + "M";
+                EditorUtility.DisplayProgressBar("progress", content, progress);
+            },
+                () =>
+                {
+                    Debug.Log("pack finish! 耗时:" + sp.Elapsed.TotalSeconds + "s");
+                });
+        }
+        catch (Exception e)
+        {
+            throw new Exception(e.Message);
+        }
+        finally
+        {
+            EditorUtility.ClearProgressBar();
+            AssetDatabase.Refresh();
+        }
     }
 
     static HashSet<string> GetAllAssets()
---
+++ LuaFramework/Assets/Scripts/Common/ZipUtil.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using ICSharpCode.SharpZipLib.Zip;
+
+public class ZipUtil
+{
+    /// <summary>
+    /// 压缩单个文件
+    /// </summary>
+    /// <param name="file">需要压缩的文件路径</param>
+    /// <param name="zipFile">创建压缩包的路径</param>
+    /// <param name="packLevel">压缩级别(1-9)</param>
+    /// <param name="passwd">密码</param>
+    public static void PackFileAsync(string file, string zipFile, Action<long, long> progress = null, Action complete = null, int packLevel = 6, string passwd = null)
+    {
+        Action temp = () =>
+        {
+            PackFile(file, zipFile, progress, complete, packLevel, passwd);
+        };
+        temp.BeginInvoke(null, null);
+    }
+    /// <summary>
+    /// 压缩单个文件
+    /// </summary>
+    /// <param name="file">需要压缩的文件路径</param>
+    /// <param name="zipFile">创建压缩包的路径</param>
+    /// <param name="packLevel">压缩级别(1-9)</param>
+    /// <param name="passwd">密码</param>
+    public static void PackFile(string file, string zipFile, Action<long, long> progress = null, Action complete = null, int packLevel = 6, string passwd = null)
+    {
+        if (!File.Exists(file))
+        {
+            Debug.LogError("file to pack is not found!");
+            return;
+        }
+        ZipOutputStream os = new ZipOutputStream(File.Create(zipFile));
+        ZipEntry entry = new ZipEntry(Path.GetFileName(file));
+        FileStream fs = new FileStream(file, FileMode.Open);
+        try
+        {
+            int curLength = 0;
+            entry.IsUnicodeText = true;             //防止中文乱码
+            entry.DateTime = DateTime.Now;
+            entry.Size = fs.Length;
+            os.PutNextEntry(entry);
+            os.SetLevel(packLevel);
+            os.Password = passwd;
+            Debug.Log(packLevel + " " + passwd);
+            int length = 0;
+            byte[] buffer = new byte[1024 * 1024];   //每次读取一兆的字节
+
+            while (true)
+            {
+                length = fs.Read(buffer, 0, buffer.Length);
+                if (length <= 0) break;
+                os.Write(buffer, 0, length);
+                curLength += length;
+                if (progress != null) progress(curLength, fs.Length);
+            }
+            if (complete != null) complete();
+        }
+        catch (Exception e)
+        {
+            throw new Exception(e.Message);
+        }
+        finally
+        {
+            fs.Close();
+            os.Finish();
+            os.Close();
+        }
+    }
+
+    /// <summary>
+    /// 压缩文件夹
+    /// </summary>
+    /// <param name="dir"></param>
+    /// <param name="zipFile"></param>
+    /// <param name="progress"></param>
+    /// <param name="complete"></param>
+    /// <param name="packLevel"></param>
+    /// <param name="passwd"></param>
+    public static void PackDirectoryAsync(string dir, string zipFile, Action<long, long> progress = null, Action complete = null, int packLevel = 6, string passwd = null)
+    {
+        Action temp = () =>
+        {
+            PackDirectory(dir, zipFile, progress, complete, packLevel, passwd);
+        };
+        temp.BeginInvoke(null, null);
+    }
+
+    /// <summary>
+    /// 压缩文件夹
+    /// </summary>
+    /// <param name="dir"></param>
+    /// <param name="zipFile"></param>
+    /// <param name="progress"></param>
+    /// <param name="complete"></param>
+    /// <param name="packLevel"></param>
+    /// <param name="passwd"></param>
+    public static void PackDirectory(string dir, string zipFile, Action<long, long> progress = null, Action complete = null, int packLevel = 6, string passwd = null)
+    {
+        dir = dir.Replace('/', '\\');
+        if (!dir.EndsWith("\\")) dir += "\\";
+
+        DirectoryInfo dirInfo = new DirectoryInfo(dir);
+        if (!dirInfo.Exists)
+        {
+            Debug.LogError("pack failed!" + "\n" + dir + " is not found!");
+            return;
+        }
+
+        string zipDir = Path.GetDirectoryName(zipFile);
+        DirectoryInfo zipDirInfo = new DirectoryInfo(zipDir);
+        if (!zipDirInfo.Exists) zipDirInfo.Create();
+
+        long totalSize = dirInfo.GetFiles("*", SearchOption.AllDirectories).Sum(info => info.Length);
+        long readSize = 0;
+
+        string[] files = Directory.GetFiles(dir, "*", SearchOption.AllDirectories);
+
+        if (File.Exists(zipFile)) File.Delete(zipFile);
+
+        ZipOutputStream os = new ZipOutputStream(File.Create(zipFile));
+
+        try
+        {
+            foreach (var file in files)
+            {
+                if (file.EndsWith(".meta")) continue;
+                FileStream fs = File.Open(file, FileMode.Open);
+                ZipEntry entry = new ZipEntry(file.Replace(dir, ""));
+                entry.IsUnicodeText = true;
+                entry.DateTime = DateTime.Now;
+                entry.Size = fs.Length;
+                os.PutNextEntry(entry);
+                os.SetLevel(packLevel);
+                os.Password = passwd;
+                byte[] buffer = new byte[1024 * 1024];
+                while (true)
+                {
+                    var length = fs.Read(buffer, 0, buffer.Length);
+                    if (length <= 0) break;
+                    readSize += length;
+                    os.Write(buffer, 0, length);
+                    if (progress != null) progress(readSize, totalSize);
+                }
+                fs.Close();
+                fs.Dispose();
+            }
+            if (complete != null) complete();
+        }
+        catch (Exception e)
+        {
+            throw new Exception(e.Message);
+        }
+        finally
+        {
+            os.Finish();
+            os.Close();
+        }
+    }
+
+    /// <summary>
+    /// 解压缩包
+    /// </summary>
+    /// <param name="dir"></param>
+    /// <param name="zipFile"></param>
+    /// <param name="progress"></param>
+    /// <param name="complete"></param>
+    /// <param name="passwd"></param>
+    public static void UnpackFileAsync(string dir, string zipFile, Action<long, long> progress = null, Action complete = null, string passwd = null)
+    {
+        Action temp = () =>
+        {
+            UnpackFile(dir, zipFile, progress, complete, passwd);
+        };
+        temp.BeginInvoke(null, null);
+    }
+
+    /// <summary>
+    /// 解压缩包
+    /// </summary>
+    /// <param name="dir"></param>
+    /// <param name="zipFile"></param>
+    /// <param name="progress"></param>
+    /// <param name="complete"></param>
+    /// <param name="passwd"></param>
+    public static void UnpackFile(string dir, string zipFile, Action<long, long> progress = null, Action complete = null, string passwd = null)
+    {
+        Debug.Log("begin unpack file");
+        dir = dir.Replace('/', '\\');
+        if (!dir.EndsWith("\\")) dir += "\\";
+        FileInfo info = new FileInfo(zipFile);
+        if (!info.Exists)
+        {
+            Debug.LogError("unpack failed!" + "\n" + zipFile + " is not found!");
+            return;
+        }
+        ZipInputStream reader = new ZipInputStream(File.Open(zipFile, FileMode.Open));
+        ZipEntry entry = null;
+        long totalSize = info.Length;
+        long readSize = 0;
+        try
+        {
+            while ((entry = reader.GetNextEntry()) != null)
+            {
+                entry.IsUnicodeText = true;
+
+                if (entry.IsDirectory) continue;
+
+                string dir_part = Path.GetDirectoryName(entry.Name);
+                dir_part = dir_part.Replace('/', '\\');
+                if (!dir_part.EndsWith("\\")) dir_part += "\\";
+
+                string fullDir = dir + dir_part;
+
+                if (!Directory.Exists(fullDir)) Directory.CreateDirectory(fullDir);
+
+                string filePath = fullDir + Path.GetFileName(entry.Name);
+
+                if (File.Exists(filePath)) File.Delete(filePath);
+
+                FileStream fs = new FileStream(filePath, FileMode.CreateNew);
+
+                byte[] buffer = new byte[1024 * 1024];
+                int length = 0;
+                while (true)
+                {
+                    length = reader.Read(buffer, 0, buffer.Length);
+                    if (length <= 0) break;
+                    fs.Write(buffer, 0, length);
+                    if (progress != null) progress(readSize, totalSize);
+                }
+                readSize += entry.CompressedSize;
+                fs.Close();
+                fs.Dispose();
+            }
+            if (complete != null) complete();
+        }
+        catch (Exception e)
+        {
+            throw new Exception(e.Message);
+        }
+        finally
+        {
+            reader.Close();
+            reader.Dispose();
+        }
+    }
+}
